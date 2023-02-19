e04dc42418da18d9ac2f3a58c355d9af02b8fcfa
ybayoumy <yassin.omgb@yahoo.com>
2023-02-18 21:06:27 -0700

    Camera class

---
+++ src/Camera.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+
+class Camera {
+public:
+
+	Camera(float t, float p, float r);
+
+	glm::mat4 getView();
+	glm::vec3 getPos();
+	void incrementTheta(float dt);
+	void incrementPhi(float dp);
+	void incrementR(float dr);
+
+private:
+
+	float theta;
+	float phi;
+	float radius;
+};
